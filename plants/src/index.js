2ce7f9d9172145d3abac25f11fbc43cf362344c3
NiutaR <69516148+NiutaR@users.noreply.github.com>
2023-02-05 20:28:15 +0200

    init: start project

---
+++ plants/src/index.js
@@ -0,0 +1,12 @@
+//get the servive-btns
+
+const gardenersBtn = document.getElementById('gardens');
+const lawnBtn = document.getElementById('lawn');
+const plantingBtn = document.getElementById('planting');
+
+//get the cards 
+const gardenCards = document.querySelectorAll('.garden-card');
+const plantingCards = document.querySelectorAll('.planting-card');
+const lawnCard = document.querySelector('.lawn-card');
+
+
